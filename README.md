b8fb043066497f266eb20b74c03bb6c57cd06512
bismarkamanor <bismarkokleteyamanor@gmail.com>
2022-04-13 12:38:10 +0000

    Code Update - Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
-## Skippy - Youtube Ads Skipper Chrome Extension
+# Skippy - Youtube Ads Skipper Chrome Extension
+
+Skippy is chrome extension that automatically skips Ads that pop-up while watching youtube videos.
+
+<!-- show image on readme -->
+
+![](https://raw.githubusercontent.com/siddharthkp/skippy/master/skippy.png)
+
+## Installation
+
+To use Skippy, kindly download it from the [chrome web store](https://chrome.google.com/webstore).
+
+## How it Works
+
+Content Script
+
+```javascript
+// content script
+function autoSkip() {
+  // set interval to check for existence of "Skip now" button every 500ms i.e 0.5s or half a second
+  setInterval(() => {
+    //   get the skip button
+    var skipButton = document.getElementsByClassName("ytp-ad-skip-button");
+    //   if skip button exists
+    if (skipButton.length > 0) {
+      //   click the skip button
+      skipButton[0].click();
+    }
+  }, 500);
+}
+```
+
+Like most extensions, Skippy injects a content script file like the above into the youtube page.
+
+Once injected, the content script listens for the existence of the `Skip now` button and clicks it whenever it is found.
+
+This is done by using the `setInterval` function which executes the function every 500ms i.e 0.5s or half a second.
+
+## Roadmap
+
+We are currently working on improving the extension.
+
+Some of the things we are working on include:
+
+    * Skippy for other browsers like Firefox, Opera, Safari, etc.
+
+    * Hide Ads on other websites like Amazon, Flipkart, etc.
+
+## Contributing
+
+Pull requests are welcome!
+
+For major changes, please open an issue to discuss what you would like to change.
+
+Test your changes before submitting them.
+
+Kindly follow the [contributing guidelines]() to ensure your contribution is well-written.
+
+## Author
+
+Hey, I'm Bismark Okletey. I'm a software developer and the creator of Skippy.
+
+I look forward to acknowledging you for your contributions.
+
+## License
+
+<!-- add MIT LICENCE with link to LICENCE -->
+
+Skippy is licensed under the MIT License.
--- content.js
+++ content.js
@@ -1,3 +1,85 @@
+// encode url
+function encodeUrlHandler(uri) {
+  let encodedUri = encodeURI(uri);
+  return encodedUri;
+}
+
+// function to create a popup card
+function createPopupCard() {
+  // create the popup card
+  var popupCard = document.createElement("div");
+  popupCard.className = "skippy-card-container";
+  popupCard.id = "skippy-card-container";
+
+  // create the popup card header
+  var popupCardHeader = document.createElement("div");
+  popupCardHeader.className = "skippy-card-header";
+
+  // create the popup card header left side
+  var popupCardHeaderLeft = document.createElement("div");
+  popupCardHeaderLeft.className = "skippy-card-header-left";
+
+  // create the popup card header image
+  var popupCardHeaderImage = document.createElement("img");
+  popupCardHeaderImage.src = encodeUrlHandler(
+    chrome.runtime.getURL("images/icon_150.png")
+  );
+
+  // create the popup card header left inner
+  var popupCardHeaderLeftInner = document.createElement("div");
+  popupCardHeaderLeftInner.className = "skippy-card-header-left-inner";
+
+  // create the popup card header left inner title
+  var popupCardHeaderLeftInnerTitle = document.createElement("h2");
+  popupCardHeaderLeftInnerTitle.innerText = "Skippy";
+
+  // create the popup card header left inner subtitle
+  var popupCardHeaderLeftInnerSubtitle = document.createElement("p");
+  popupCardHeaderLeftInnerSubtitle.innerText = "YouTube Ads Skipper";
+
+  //  create the popup card content
+  var popupCardContent = document.createElement("div");
+  popupCardContent.className = "skippy-card-content";
+
+  // create the popup card content paragraph
+  var popupCardContentParagraph = document.createElement("p");
+  popupCardContentParagraph.innerHTML =
+    "<strong>Skippy</strong> automatically skips Ads in youtube videos so you dont have to click the <strong>skip now</strong> button anymore 😃. ";
+
+  // append the elements to the popup card
+  popupCardHeaderLeftInner.appendChild(popupCardHeaderLeftInnerTitle);
+  popupCardHeaderLeftInner.appendChild(popupCardHeaderLeftInnerSubtitle);
+  popupCardHeaderLeft.appendChild(popupCardHeaderImage);
+  popupCardHeaderLeft.appendChild(popupCardHeaderLeftInner);
+  popupCardHeader.appendChild(popupCardHeaderLeft);
+  popupCardContent.appendChild(popupCardContentParagraph);
+  popupCard.appendChild(popupCardHeader);
+  popupCard.appendChild(popupCardContent);
+
+  return popupCard;
+}
+
+// custom ads slot message
+function customAdsSlotMessageHandler() {
+  // get DOM elements for custom ads slot message
+  let container = document.getElementsByClassName("ytp-ad-message-container");
+  let messages = document.getElementsByClassName(
+    "ytp-ad-text ytp-ad-message-text"
+  );
+  // if custom ads slot message is present remove it
+  if (container.length > 0) {
+    for (let i = 0; i < messages.length; i++) {
+      if (messages[i].id === "ad-text:p") {
+        container[0].style.backgroundColor = "#ff0000 !important";
+        container[0].style.color = "#ffffff !important";
+        messages[i].innerText = "Skippy";
+        messages[i].style.backgroundColor = "#ff0000 !important";
+        messages[i].style.color = "#ffffff !important";
+      }
+    }
+  }
+}
+
 function skippyIcon() {
   let mastHeadContainer = document.getElementsByClassName(
     "style-scope ytd-masthead"
@@ -13,15 +95,29 @@ function skippyIcon() {
     document.getElementsByTagName("head")[0].appendChild(link);
     // inject skippy icon into the navbar
     if (mastHeadContainer[i].id == "end") {
-      let skippyIcon = document.createElement("img");
-      skippyIcon.src = chrome.runtime.getURL("images/icon_150.png");
-      //   add class to skippy icon
-      skippyIcon.className = "skippy-icon";
-      // inject
-      mastHeadContainer[i].insertBefore(
-        skippyIcon,
-        mastHeadContainer[i].childNodes[0]
-      );
+      // search if skippy icon is already present
+      setInterval(() => {
+        let exists = document.getElementsByClassName("skippy-icon");
+        if (exists.length === 0) {
+          let skippyPopupContainer = document.createElement("div");
+          skippyPopupContainer.className = "skippy-popup-container";
+          skippyPopupContainer.id = "skippy-popup-container";
+          let skippyIcon = document.createElement("img");
+          skippyIcon.src = encodeUrlHandler(
+            chrome.runtime.getURL("images/icon_150.png")
+          );
+          //   add class to skippy icon
+          skippyIcon.className = "skippy-icon";
+          skippyPopupContainer.appendChild(skippyIcon);
+          // append popup card to the skippy icon
+          skippyPopupContainer.appendChild(createPopupCard());
+          // inject
+          mastHeadContainer[i].insertBefore(
+            skippyPopupContainer,
+            mastHeadContainer[i].childNodes[0]
+          );
+        }
+      }, 500);
     }
   }
 }
@@ -40,9 +136,14 @@ function autoSkip() {
     let addOverlayButton = document.getElementsByClassName(
       "ytp-ad-overlay-close-button"
     );
+
+    // custom ads slot message
+    customAdsSlotMessageHandler();
+
     //  if skip button is present click it
     if (ytSkipBtnContainer.length > 0) {
       if (ytSkipBtn.length > 0) {
+        ytSkipBtn[0].style.backgroundColor = "#ff0000";
         ytSkipBtn[0].click();
       }
       //   close add overlay popup if present
@@ -53,5 +154,10 @@ function autoSkip() {
   }, 500);
 }
 
-skippyIcon();
+document.onreadystatechange = function () {
+  if (document.readyState == "complete") {
+    skippyIcon();
+  }
+};
+
 autoSkip();
